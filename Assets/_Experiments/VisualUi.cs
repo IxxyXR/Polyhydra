003a0c6c80ac648ed2fd9992942e15b8bc7d2f62
andybak <andy@andybak.net>
2019-08-09 09:38:47 +0100

    More work on VisualUI

--- Assets/_Experiments/VisualUi.cs
+++ Assets/_Experiments/VisualUi.cs
@@ -1,31 +1,69 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿using System;
 using Conway;
 using UnityEngine;
 using Wythoff;
 
 public class VisualUi : MonoBehaviour
 {
-    public PolyHydra MainPoly;
-    public int FirstOp = 1;
-    public int LastOp = 12;
-    public int FirstPoly = 1;
-    public int LastPoly = 8;
+    public PolyHydra PolyPrefab;
+    public GameObject Centerpoint;
+    private int CurrentPage = 0;
+    private int NumPolyPages;
+    private int NumOpPages;
+    private int LastPolyIndex;
+    private int LastOpIndex;
     public float radius = 2.2f;
-    
-    void Start()
+    public int ItemsPerPage = 12;
+
+    public enum Menus
     {
-        ShowBasePolys();
+        Polys,
+        Ops,
     }
 
-    void ShowOps()
+    private Menus CurrentMenu;
+
+    private GameObject _pivot;
+
+    void Start()
     {
-        float numOps = LastOp - FirstOp + 1;
-        for (int i = FirstOp; i <= LastOp; i++)
+        LastPolyIndex = Uniform.Uniforms.Length;
+        LastOpIndex = Enum.GetValues(typeof(PolyHydra.Ops)).Length;
+        NumPolyPages = LastPolyIndex / ItemsPerPage;
+        NumOpPages = LastOpIndex / ItemsPerPage;
+        RecreatePivot();
+    }
+
+    void RecreatePivot()
+    {
+        if (_pivot != null)
         {
-            float x = Mathf.Sin((i / numOps) * Mathf.PI * 2) * radius;
-            float y = Mathf.Cos((i / numOps) * Mathf.PI * 2) * radius;
-            GameObject copy = Instantiate(MainPoly.gameObject, new Vector3(x, y, 0), Quaternion.identity);
+            Destroy(_pivot);
+        }
+        _pivot = new GameObject();
+        _pivot.transform.parent = Centerpoint.transform;
+        _pivot.name = "MenuPivot";
+    }
+
+    void ClearMenu()
+    {
+        RecreatePivot();
+    }
+
+    void ShowConwayMenu()
+    {
+        ClearMenu();
+
+        int firstIndex = CurrentPage * ItemsPerPage;
+        int lastIndex = firstIndex + ItemsPerPage - 1;
+        lastIndex = Math.Min(lastIndex, LastOpIndex);
+        Debug.Log($"Page: {CurrentPage} firstIndex: {firstIndex} lastIndex: {lastIndex}");
+        for (int i = firstIndex; i <= lastIndex; i++)
+        {
+            float x = Mathf.Sin(((i - firstIndex) / (float)ItemsPerPage) * Mathf.PI * 2) * radius;
+            float y = Mathf.Cos(((i - firstIndex) / (float)ItemsPerPage) * Mathf.PI * 2) * radius;
+//            Debug.Log($"{i}: {x},{y}");
+            GameObject copy = Instantiate(PolyPrefab.gameObject, new Vector3(x, y, 0), Quaternion.identity, _pivot.transform);
             copy.transform.localScale = Vector3.one / 2f;
             var copyPoly = copy.GetComponent<PolyHydra>();
             //copyPoly.ConwayOperators.Clear();
@@ -44,29 +82,80 @@ public class VisualUi : MonoBehaviour
         }
     }
 
-    void ShowBasePolys()
+    void ShowUniformMenu()
     {
-        float numOps = LastPoly - FirstPoly + 1;
-        for (int i = FirstPoly; i <= LastPoly; i++)
+        ClearMenu();
+        int firstIndex = CurrentPage * ItemsPerPage;
+        int lastIndex = firstIndex + ItemsPerPage - 1;
+        lastIndex = Math.Min(lastIndex, LastPolyIndex);
+        Debug.Log($"Page: {CurrentPage} firstIndex: {firstIndex} lastIndex: {lastIndex}");
+        for (int i = firstIndex; i <= lastIndex; i++)
         {
-            float x = Mathf.Sin((i / numOps) * Mathf.PI * 2) * radius;
-            float y = Mathf.Cos((i / numOps) * Mathf.PI * 2) * radius;
-            GameObject copy = Instantiate(MainPoly.gameObject, new Vector3(x, y, 0), Quaternion.identity);
+            float x = Mathf.Sin(((i - firstIndex) / (float)ItemsPerPage) * Mathf.PI * 2) * radius;
+            float y = Mathf.Cos(((i - firstIndex) / (float)ItemsPerPage) * Mathf.PI * 2) * radius;
+            GameObject copy = Instantiate(PolyPrefab.gameObject, new Vector3(x, y, 0), Quaternion.identity, _pivot.transform);
             copy.transform.localScale = Vector3.one / 2f;
             var copyPoly = copy.GetComponent<PolyHydra>();
             //copyPoly.ConwayOperators.Clear();
             copyPoly.UniformPolyType = (PolyTypes) i;
             var uniform = Uniform.Uniforms[i];
             if (uniform.Wythoff == "-") continue;
-            var wythoff = new WythoffPoly(uniform.Wythoff);
+            var wythoff = new WythoffPoly(uniform.Wythoff.Replace("p", "5").Replace("q", "2"));
             // Which types to create? Example:
-            if (wythoff.SymmetryType != 3) continue;
+            //if (wythoff.SymmetryType != 3) continue;
             copyPoly.MakePolyhedron();
         }
     }
 
     void Update()
     {
-        
+        if (_pivot != null) _pivot.transform.Rotate(0, .1f, 0);
+        if (Input.GetKeyDown(KeyCode.P))
+        {
+            ChangeMenu(-1);
+        }
+        else if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            ChangeMenu(1);
+        }
+        else if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            ChangePage(-1);
+        }
+        else if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            ChangePage(1);
+        }
+    }
+
+    private void ChangeMenu(int direction)
+    {
+        CurrentMenu += direction;
+        CurrentMenu = (Menus)((int)CurrentMenu % Enum.GetValues(typeof(Menus)).Length);
+        switch (CurrentMenu)
+        {
+            case Menus.Ops:
+                ShowConwayMenu();
+                break;
+            case Menus.Polys:
+                ShowUniformMenu();
+                break;
+        }
+    }
+
+    private void ChangePage(int direction)
+    {
+        CurrentPage += direction;
+        switch (CurrentMenu)
+        {
+            case Menus.Ops:
+                CurrentPage %= NumOpPages;
+                ShowConwayMenu();
+                break;
+            case Menus.Polys:
+                CurrentPage %= NumPolyPages;
+                ShowUniformMenu();
+                break;
+        }
     }
 }
