9b15d1a0896d72863f377ee0ef3f639ce2c0eea4
andybak <andy@andybak.net>
2019-08-10 14:02:24 +0100

    Move screenshot code into scripts

---
+++ Assets/_Scripts/ScreenShot.cs
@@ -0,0 +1,54 @@
+using System.Linq;
+using UnityEngine;
+using System.Collections;
+using System.IO;
+using UnityEditor.Recorder;
+using UnityEditor.Recorder.Input;
+
+
+public class ScreenShot : MonoBehaviour
+{
+   RecorderController m_RecorderController;
+   public GameObject HideMe;
+         
+   void OnEnable()
+   {
+       var controllerSettings = ScriptableObject.CreateInstance<RecorderControllerSettings>();
+       m_RecorderController = new RecorderController(controllerSettings);
+
+       // Image
+       var imageRecorder = ScriptableObject.CreateInstance<ImageRecorderSettings>();
+       imageRecorder.name = "My Image Recorder";
+       imageRecorder.enabled = true;
+       imageRecorder.outputFormat = ImageRecorderOutputFormat.JPEG;
+       imageRecorder.captureAlpha = false;
+       imageRecorder.outputFile = Path.Combine(Application.persistentDataPath, "Screenshots", "image_") + DefaultWildcard.Take;
+
+       imageRecorder.imageInputSettings = new GameViewInputSettings
+       {
+           outputWidth = 2160,
+           outputHeight = 2160,
+       };
+
+       // Setup Recording
+       controllerSettings.AddRecorderSettings(imageRecorder);
+       controllerSettings.SetRecordModeToSingleFrame(0);
+   }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.LeftShift) && Input.GetKeyDown(KeyCode.S))
+        {
+            HideMe.SetActive(false);
+            m_RecorderController.StartRecording();
+            StartCoroutine(WaitForCapture());
+        }
+    }
+    
+    IEnumerator WaitForCapture()
+    {
+        yield return new WaitWhile(() => m_RecorderController.IsRecording());
+        Debug.Log($"Saved to {m_RecorderController.settings.recorderSettings.First().outputFile}");
+        HideMe.SetActive(true);
+    }
+}
