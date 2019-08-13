e457ea28c1003c0ac63a110a2122f8d502e2a111
Andy Baker <andy@andybak.net>
2019-08-13 10:47:37 +0100

    Screenshot keyboard fix

--- Assets/_Scripts/ScreenShot.cs
+++ Assets/_Scripts/ScreenShot.cs
@@ -9,6 +9,10 @@ using UnityEditor.Recorder.Input;
 public class ScreenShot : MonoBehaviour
 {
    RecorderController m_RecorderController;
+   [Tooltip("Press Shift + this to trigger a screenshot")]
+   public KeyCode ShortcutKey = KeyCode.S;
+   public string FolderName = "Screenshots";
+   public string FilenamePrefix = "screenshot_";
    public GameObject HideMe;
          
    void OnEnable()
@@ -22,7 +26,7 @@ public class ScreenShot : MonoBehaviour
        imageRecorder.enabled = true;
        imageRecorder.outputFormat = ImageRecorderOutputFormat.JPEG;
        imageRecorder.captureAlpha = false;
-       imageRecorder.outputFile = Path.Combine(Application.persistentDataPath, "Screenshots", "image_") + DefaultWildcard.Take;
+       imageRecorder.outputFile = Path.Combine(Application.persistentDataPath, FolderName, FilenamePrefix) + DefaultWildcard.Take;
 
        imageRecorder.imageInputSettings = new GameViewInputSettings
        {
@@ -37,8 +41,9 @@ public class ScreenShot : MonoBehaviour
 
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.LeftShift) && Input.GetKeyDown(KeyCode.S))
+        if (Input.GetKey(KeyCode.LeftShift) && Input.GetKeyDown(ShortcutKey))
         {
+            Debug.Log("terterter");
             HideMe.SetActive(false);
             m_RecorderController.StartRecording();
             StartCoroutine(WaitForCapture());
