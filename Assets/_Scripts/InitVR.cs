1cb94322148556e9344d074a0c3e6712cbc98fa1
Andy Baker <andy@andybak.net>
2019-12-30 18:44:40 +0000

    VR tweaks

--- Assets/_Scripts/InitVR.cs
+++ Assets/_Scripts/InitVR.cs
@@ -5,11 +5,16 @@ using UnityEngine;
 public class InitVR : MonoBehaviour
 {
     public Transform PolyRoot;
+    public Transform AkaiMarker;
+    private AkaiPrefabController _akaiPrefabController;
     
     void Start()
     {
         // Quick Hacks to configure for VR
         PolyRoot.transform.localScale = Vector3.one * 0.75f;
+        _akaiPrefabController = FindObjectOfType<AkaiPrefabController>();
+        _akaiPrefabController.transform.position = AkaiMarker.position;
+        _akaiPrefabController.transform.rotation = AkaiMarker.rotation;
     }
 
 }
