1c58912d647b8aee771a30785bf41179e6da2b8b
andybak <andy@andybak.net>
2020-01-14 13:16:26 +0000

    Audio reactive

---
+++ Assets/_Experiments/Audio Reactive/PolyAudio.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+
+public class PolyAudio : MonoBehaviour
+{
+
+    public int OpIndex;
+    public float Scale = -0.05f;
+    public float Offset = 0.5f;
+    public int RenderEvery = 3;
+    //private int Width = 200;
+    
+    
+    //private float[] _array;
+    private PolyHydra poly;
+    private float amount;
+    
+    
+    void Start()
+    {
+        poly = GetComponent<PolyHydra>();
+        //_array = new float[Width];
+    }
+
+    void Update()
+    {
+        if (Time.frameCount % RenderEvery != 0) return;
+        //Lasp.MasterInput.RetrieveWaveform(Lasp.FilterType.LowPass, _array);
+        float peak = Lasp.MasterInput.GetPeakLevelDecibel(Lasp.FilterType.LowPass);
+        //float avg = _array.Select(x=>Mathf.Abs(x)).Average();
+        amount = Mathf.Abs(peak) * Scale + Offset;
+        var op = poly.ConwayOperators[OpIndex];
+        op.amount = amount;
+        poly.ConwayOperators[OpIndex] = op;
+        poly.Rebuild();
+    }
+}
