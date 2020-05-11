8400523458a717fe764b6ed6702f2b469026255d
Andy Baker <andy@andybak.net>
2020-05-11 01:29:39 +0100

    Better

--- Assets/_Experiments/CloneAndVary/CloneAndVary.cs
+++ Assets/_Experiments/CloneAndVary/CloneAndVary.cs
@@ -1,8 +1,7 @@
 ﻿using System;
 using System.Collections;
-using System.Collections.Generic;
-using System.Data;
 using UnityEngine;
+
 
 [ExecuteInEditMode]
 public class CloneAndVary : MonoBehaviour
@@ -10,78 +9,100 @@ public class CloneAndVary : MonoBehaviour
     public int rows = 10;
     public int columns = 10;
     public float spacing = 3f;
+    public float xOffset = 0f;
+    public float yOffset = 0f;
     public float xMagnitude = 0.1f;
     public float yMagnitude = 0.1f;
     public Material material;
+    public Transform[,] clones;
 
-    private PolyHydra poly;
+    public PolyHydra poly;
 
     public bool dummy;
 
     void Start()
     {
+        Reset();
     }
 
-    void FindPoly()
+    [ContextMenu("Reset")]
+    public void Reset()
     {
-        poly = FindObjectOfType<PolyHydra>();
+        clones = null;
+        Generate();
     }
 
-    void Update()
+    private void Update()
     {
+        Generate();
     }
-
+    
     private void OnValidate()
     {
         Generate();
     }
 
+    private Transform InitClone(int x, int y)
+    {
+        var go = Instantiate(
+            poly,
+            GetClonePosition(x, y),
+            Quaternion.identity,
+            transform);
+        clones[x, y] = go.transform;
+        return go.transform;
+
+    }
+
     public void Generate()
     {
-        if (poly == null)
+        if (clones==null || clones.GetLength(0) != rows || clones.GetLength(1) != columns)
         {
-            FindPoly();
-        }
-        
-        foreach (Transform child in transform)
-        {
-            if (Application.isPlaying)
+            foreach (Transform child in transform)
             {
-                Destroy(child.gameObject);
-            }
-            else
-            {
-                UnityEditor.EditorApplication.delayCall += () =>
-                {
-                    DestroyImmediate(child.gameObject);
-                }; 
-            }
-        }
-        
-        for (int y = 0; y < rows; y++)
-        {
-            for (int x = 0; x < columns; x++)
-            {
-                var op = poly.ConwayOperators[0];
-                op.amount = x * xMagnitude;
-                op.amount2 = y * yMagnitude;
-                poly.ConwayOperators[0] = op;
-                poly.Rebuild();
-                var go = new GameObject();
-                var mf = go.AddComponent<MeshFilter>();
-                var mr = go.AddComponent<MeshRenderer>();
-                mr.material = material;
-                go.transform.position = new Vector3(x * spacing, 1, y * spacing);
-                go.transform.parent = transform;
                 if (Application.isPlaying)
                 {
-                    mf.mesh = poly.GetComponent<MeshFilter>().mesh;
+                    Destroy(child.gameObject);
                 }
                 else
                 {
-                    mf.sharedMesh = poly.GetComponent<MeshFilter>().sharedMesh;
+                    StartCoroutine(Destroy(child.gameObject));
                 }
             }
+            clones = new Transform[rows, columns];
         }
+
+        for (int y = 0; y < columns; y++)
+        {
+            for (int x = 0; x < rows; x++)
+            {
+                var clone = clones[x, y];
+                if (clone == null)
+                {
+                    clone = InitClone(x, y);
+                }
+                else
+                {
+                    clone.position = GetClonePosition(x, y);
+                }
+                var p = clone.GetComponent<PolyHydra>();
+                var op = p.ConwayOperators[0];
+                op.amount = xOffset + x * xMagnitude;
+                op.amount2 = yOffset + y * yMagnitude;
+                p.ConwayOperators[0] = op;
+                p.Rebuild();
+            }
+        }
+    }
+
+    private Vector3 GetClonePosition(int x, int y)
+    {
+        return new Vector3(x * spacing, 1, y * spacing);
+    }
+    
+    IEnumerator Destroy(GameObject go)
+    {
+        yield return new WaitForEndOfFrame();
+        DestroyImmediate(go);
     }
 }
