a8a0b6c23a28d3dd9c9fe8b47feb46d3deec91a4
Andy Baker <andy@andybak.net>
2021-07-21 02:13:09 +0100

    Unity version bump

--- Assets/_Experiments/PolyProbuilder/Editor/CreateWythoff.cs
+++ Assets/_Experiments/PolyProbuilder/Editor/CreateWythoff.cs
@@ -91,13 +91,13 @@ sealed class CreateWythoff : MenuAction
         // extrudeMethod = (ExtrudeMethod) EditorGUILayout.EnumPopup("Extrude Method", extrudeMethod);
         // if (EditorGUI.EndChangeCheck()) EditorPrefs.SetInt("pb_CreateShadowObject_extrudeMethod", (int) extrudeMethod);
 
-        if (EditorGUI.EndChangeCheck()) DoAction();
+        if (EditorGUI.EndChangeCheck()) PerformAction();
 
         GUILayout.FlexibleSpace();
 
         if (GUILayout.Button("Create Wythoff Polyhedra"))
         {
-            DoAction();
+            PerformAction();
             SceneView.RepaintAll();
 //				MenuOption.CloseAll();
         }
@@ -150,7 +150,7 @@ sealed class CreateWythoff : MenuAction
     /// Perform the action.
     /// </summary>
     /// <returns>Return a pb_ActionResult indicating the success/failure of action.</returns>
-    public override ActionResult DoAction()
+    protected override ActionResult PerformActionImplementation()
     {
 
         //var prev = GameObject.Find("New Polyhedra");
